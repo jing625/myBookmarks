0c36af4709463337f26edfaaa056dc1b2a0b740f
lgj <469779397@qq.com>
2018-08-09 12:05:37 +0800

    无缝的轮播

---
+++ slides-demo/main.js
@@ -0,0 +1,47 @@
+let n
+first()
+setInterval(() => {
+  makeLeave(getImage(n))
+    .one('transitionend', (e) => {
+      makeEnter($(e.currentTarget))
+    })
+  makeCurrent(getImage(n + 1))
+  n += 1
+}, 3000)
+
+
+
+
+
+
+/* --------- */
+
+function x (n) {
+  if (n > 5) {
+    n = n % 5
+    if (n === 0) {
+      n = 5
+    }
+  } // n = 1 2 3 4 5
+  return n
+}
+
+function getImage (n) {
+  return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function first () {
+  n = 1
+  $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function makeCurrent ($node) {
+  return $node.removeClass('leave enter').addClass('current')
+}
+function makeLeave ($node) {
+  return $node.removeClass('current enter').addClass('leave')
+}
+function makeEnter ($node) {
+  return $node.removeClass('current leave').addClass('enter')
+}
